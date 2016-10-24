3848ad66571ebbe1e748ba930159a88e1e0cd27c
dino-zorovic <dino.zorovic@gmail.com>
2016-10-24 04:05:19 +0200

    Update fetch.php

--- database/fetch.php
+++ database/fetch.php
@@ -1,36 +1,3 @@
-<!doctype html>
-
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-
-  <title>MySQL</title>
-
-<?php
-/*
-Demonstrate using PHP how you would connect to a MySQL or
-
-PostgreSQL database and query for a specific record with the following
-
-fields: “id_user”, “name”, “age”, “job_title”, “inserted_on”,
-
-“last_updated”) from a table called 'users', using the ‘id_user’ field.
-
-Indicate how you would create the table taking into consideration that we
-
-are going to access to the table data only by ‘id_user’. Provide an
-
-example of how you would write a record in this same table with the user
-
-data that comes from a form send by an HTTP POST request. Don’t need
-
-to create the form, but you can create it if it helps you.*/
-    
-?>
-
-</head>
-
-<body>
 <?php
     include 'connect.php';
     $user_id_form = $_POST['user_id'];
@@ -49,5 +16,3 @@ while($row = mysqli_fetch_array($result)) {
 
 mysqli_close($conn);
 ?>
-</body>
-</html>
