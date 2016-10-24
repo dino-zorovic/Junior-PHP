0892e7d8d7aaee807f38b754639cdca36a00f82c
dino-zorovic <dino.zorovic@gmail.com>
2016-10-24 04:46:18 +0200

    Update insert.php

--- database/insert.php
+++ database/insert.php
@@ -4,7 +4,7 @@ include 'connect.php';
 $name = $_POST['name'];
 $age = $_POST['age'];
 $job_title = $_POST['job_title'];
-$isql = "INSERT INTO ma (name, age, job_title) VALUES ('".$name."', '".$age."', '".$job_title."')";
+$isql = "INSERT INTO users (name, age, job_title) VALUES ('".$name."', '".$age."', '".$job_title."')";
    
 if (mysqli_query($conn, $isql)) {
     echo "New record created successfully";
